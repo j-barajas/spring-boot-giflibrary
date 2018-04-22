a2ef6780c349d2a9d9ea49dc2e9b2bbd639731db
Jorge Barajas <jbdevbot@gmail.com>
2018-04-22 17:06:45 -0500

    Configure setup

---
+++ src/main/java/com/barajas/giflib/AppConfig.java
@@ -0,0 +1,17 @@
+package com.barajas.giflib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+
+/**
+ * Created by jbara on 4/22/2018.
+ */
+
+
+@EnableAutoConfiguration
+public class AppConfig {
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfig.class, args);
+    }
+}
---
+++ src/main/java/com/barajas/giflib/controller/GifController.java
@@ -0,0 +1,12 @@
+package com.barajas.giflib.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by jbara on 4/22/2018.
+ */
+
+@Controller
+public class GifController {
+
+}
