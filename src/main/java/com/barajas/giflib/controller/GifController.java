ffb6feede9f9abf2f575f90b4dfabfac71067853
Jorge Barajas <jbdevbot@gmail.com>
2018-04-23 13:14:31 -0500

    Add gif model and controller

--- src/main/java/com/barajas/giflib/controller/GifController.java
+++ src/main/java/com/barajas/giflib/controller/GifController.java
@@ -1,26 +1,31 @@
 package com.barajas.giflib.controller;
 
+import com.barajas.giflib.model.Gif;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDate;
 
 /**
  * Created by jbara on 4/22/2018.
  */
 
-    @Controller
-    public class GifController {
-        @RequestMapping(value="/")
-        public String listGifs(){
-            return "home";
-        }
-
-    @Controller
-    public class GifInfo {
-        @RequestMapping(value = "/gif")
-        @ResponseBody
-        public String listGifs() {
-            return "GIF info page";
-        }
+@Controller
+public class GifController {
+    @RequestMapping(value="/")
+    public String listGifs(){
+        return "home";
     }
+
+
+    @RequestMapping(value = "/gif")
+    public String gifDetails(ModelMap modelMap) {
+        Gif gif = new Gif("compiler-bot", LocalDate.of(2015,2,13),"Jorge Barajas",true);
+        modelMap.put("gif", gif);
+        return "gif-details";
+    }
+
 }
+
