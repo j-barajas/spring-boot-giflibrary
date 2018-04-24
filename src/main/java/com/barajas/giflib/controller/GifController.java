5a5ab2f8334adecbab2db231618513adebf9f097
j-barajas <jbdevbot@gmail.com>
2018-04-24 16:58:13 -0500

    Add path variables to controller

--- src/main/java/com/barajas/giflib/controller/GifController.java
+++ src/main/java/com/barajas/giflib/controller/GifController.java
@@ -1,8 +1,11 @@
 package com.barajas.giflib.controller;
 
+import com.barajas.giflib.data.GifRepository;
 import com.barajas.giflib.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -14,14 +17,18 @@ import java.time.LocalDate;
 
 @Controller
 public class GifController {
+    @Autowired
+    private GifRepository gifRepository;
+
     @RequestMapping("/")
     public String home(){
         return "home";
     }
 
-    @RequestMapping("/gif")
-    public String gifDetails(ModelMap modelMap) {
-        Gif gif = new Gif("compiler-bot",LocalDate.of(2018,04,23),"Jorge",true);
+    //@PathVariable  URL=> http://localhost:8080/gif/android-explosion
+    @RequestMapping("/gif/{name}")
+    public String gifDetails(@PathVariable String name, ModelMap modelMap) {
+        Gif gif = gifRepository.findByName(name);
         modelMap.put("gif", gif);
         return "gif-details";
     }
