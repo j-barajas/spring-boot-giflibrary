24840dac71b8bf24ba74979cb280e63acb495e12
j-barajas <jbdevbot@gmail.com>
2018-04-24 19:25:49 -0500

    Add category repository

---
+++ src/main/java/com/barajas/giflib/data/CategoryRepository.java
@@ -0,0 +1,39 @@
+package com.barajas.giflib.data;
+
+import com.barajas.giflib.model.Category;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by jbara on 4/24/2018.
+ */
+
+@Component
+public class CategoryRepository {
+    private static final List<Category> ALL_CATAGORIES = Arrays.asList(
+            new Category(1,"Technology"),
+            new Category(2,"People"),
+            new Category(3,"Destruction")
+    );
+
+    /**
+     *Returns a category, stored in ALL_CATEGORIES, that matches
+     * the category id searched for.
+     * @param id the category id to be searched for
+     * @return the category matching the id
+     */
+    public Category findById(int id){
+        for (Category category:ALL_CATAGORIES) {
+            if(category.getId()==id){
+                return category;
+            }
+        }
+        return null;
+    }
+
+    public List<Category> getAllCategories(){
+        return ALL_CATAGORIES;
+    }
+}
--- src/main/java/com/barajas/giflib/model/Category.java
+++ src/main/java/com/barajas/giflib/model/Category.java
@@ -4,4 +4,27 @@ package com.barajas.giflib.model;
  * Created by jbara on 4/24/2018.
  */
 public class Category {
+    private int id;
+    private String name;
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
