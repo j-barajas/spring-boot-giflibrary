810f2811e37fb9c37f9dd4ed72de0c164cd04b2e
j-barajas <jbdevbot@gmail.com>
2018-04-24 19:24:46 -0500

    Add gif repository

--- src/main/java/com/barajas/giflib/controller/GifController.java
+++ src/main/java/com/barajas/giflib/controller/GifController.java
@@ -2,14 +2,17 @@ package com.barajas.giflib.controller;
 
 import com.barajas.giflib.data.GifRepository;
 import com.barajas.giflib.model.Gif;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDate;
+import java.util.List;
 
 /**
  * Created by jbara on 4/22/2018.
@@ -21,9 +24,13 @@ public class GifController {
     private GifRepository gifRepository;
 
     @RequestMapping("/")
-    public String home(){
+    public String listGifs(ModelMap modelMap){
+        List<Gif> allGifs = gifRepository.getAllGifs();
+        modelMap.put("gifs", allGifs);
         return "home";
     }
+
+
 
     //@PathVariable  URL=> http://localhost:8080/gif/android-explosion
     @RequestMapping("/gif/{name}")
---
+++ src/main/java/com/barajas/giflib/model/Category.java
@@ -0,0 +1,7 @@
+package com.barajas.giflib.model;
+
+/**
+ * Created by jbara on 4/24/2018.
+ */
+public class Category {
+}
